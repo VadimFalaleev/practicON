b1ced1dea862d1db9a0035d7dca55cf32913dd5a
Вадим <54228342+VadimFalaleev@users.noreply.github.com>
2023-07-22 14:50:39 +0500

    fall animation

--- PracticON/Assets/Scripts/PlayerMove.cs
+++ PracticON/Assets/Scripts/PlayerMove.cs
@@ -1,27 +1,24 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 4;
-    [SerializeField] private float jumpForce = 14;
-    [SerializeField] private float friction = 0.6f;
-    [SerializeField] private float maxSpeed = 5;
-
-    [SerializeField] private Rigidbody playerRigidbody;
-    [SerializeField] private Animator animator;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _friction;
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Animator _animator;
     //[SerializeField] private AudioSource jumpAudio;
     //[SerializeField] private AudioSource attackAudio;
 
-    private float _coyoteTime = 0.2f;// ��� ���� ��� �������� ��� ������ ����� ������ ������� ����� ������ �� �����
-    private float _jumpBufferTime = 0.2f;// ������� ��������� ������ 
-    private float _coyoteTimeCounter;// ������� ������� ������
-    private float _jumpBufferCounter;// ������� ������� ������
-    private float _jumpFramesTimer;// ��� �������� �� �����, ��� �� �� ����� ������� ����������
-
+    private float _coyoteTime = 0.2f;
+    private float _jumpBufferTime = 0.2f; 
+    private float _coyoteTimeCounter;
+    private float _jumpBufferCounter;
+    private float _jumpFramesTimer;
     private float _xAxis;
-    private bool _grounded;
     private float _xEuler = 90;
+    private bool _isGrounded;
 
     //public static PlayerMove Instance { get; private set; }
 
@@ -40,26 +37,10 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         _jumpFramesTimer += Time.deltaTime;
-
         _xAxis = Input.GetAxisRaw("Horizontal");
 
-        if (_grounded)
-        {
-            _coyoteTimeCounter = _coyoteTime;
-        }
-        else
-        {
-            _coyoteTimeCounter -= Time.deltaTime;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _jumpBufferCounter = _jumpBufferTime;
-        }
-        else
-        {
-            _jumpBufferCounter -= Time.deltaTime;
-        }
+        _coyoteTimeCounter = _isGrounded ? _coyoteTimeCounter = _coyoteTime : _coyoteTimeCounter -= Time.deltaTime;
+        _jumpBufferCounter = Input.GetKeyDown(KeyCode.Space) ? _jumpBufferCounter = _jumpBufferTime : _jumpBufferCounter -= Time.deltaTime;
 
         if (_coyoteTimeCounter > 0f && _jumpBufferCounter > 0f && _jumpFramesTimer > 0.4f)
         {
@@ -67,34 +48,45 @@ public class PlayerMove : MonoBehaviour
             _jumpBufferCounter = 0f;
             Jump();
         }
-        if (Input.GetKeyUp(KeyCode.Space) && playerRigidbody.velocity.x > 0f)
+        if (Input.GetKeyUp(KeyCode.Space) && _rigidbody.velocity.x > 0f)
         {
             _coyoteTimeCounter = 0f;
         }
+        
+        _animator.SetBool("Fall", !_isGrounded);
 
+        Rotate();
+        _animator.SetBool("Walk", Input.GetAxis("Horizontal") != 0);
+
+        // if (EventSystem.current.IsPointerOverGameObject() == false)
+        // {
+        Attack();
+        // }
+    }
+
+    private void Attack()
+    {
+        if (Input.GetMouseButtonDown(0) && _isGrounded && Input.GetAxis("Horizontal") == 0)
+        {
+            // attackAudio.Play();
+            _animator.SetTrigger("Attack");
+        }
+    }
+
+    private void Rotate()
+    {
         if (_xAxis > 0)
             _xEuler = 270f;
         else if (_xAxis < 0)
             _xEuler = 90f;
         transform.localRotation = Quaternion.Euler(0, _xEuler, 0);
-        animator.SetBool("Walk", Input.GetAxis("Horizontal") != 0);
-
-
-        // if (EventSystem.current.IsPointerOverGameObject() == false)
-        // {
-        if (Input.GetMouseButtonDown(0))
-        {
-            // attackAudio.Play();
-            animator.SetTrigger("Attack");
-        }
-        // }
     }
 
     public void Jump()
     {
         //jumpAudio.Play();
-        animator.SetTrigger("Jump");
-        playerRigidbody.AddForce(0, jumpForce, 0, ForceMode.VelocityChange);
+        _animator.SetTrigger("Jump");
+        _rigidbody.AddForce(0, _jumpForce, 0, ForceMode.VelocityChange);
     }
 
     private void FixedUpdate()
@@ -102,46 +94,41 @@ public class PlayerMove : MonoBehaviour
         float speedMultiplierInAir = 1f;
         float input = Input.GetAxis("Horizontal");
 
-        //��� �� ������ ����������� ������ �� �����
-        if (_grounded)
+        if (_isGrounded)
         {
-            playerRigidbody.AddForce(input * moveSpeed, 0, 0, ForceMode.VelocityChange);
+            _rigidbody.AddForce(input * _moveSpeed, 0, 0, ForceMode.VelocityChange);
         }
-        else// ����������� ����� 
+        else
         {
             speedMultiplierInAir = 0.2f;
-            // ������������ ������������ �������� � ������
-            if (playerRigidbody.velocity.x > maxSpeed && input > 0)
+            if (_rigidbody.velocity.x > _maxSpeed && input > 0)
             {
                 speedMultiplierInAir = 0;
             }
-            if (playerRigidbody.velocity.x < -maxSpeed && input < 0)
+            if (_rigidbody.velocity.x < -_maxSpeed && input < 0)
             {
                 speedMultiplierInAir = 0;
             }
-            playerRigidbody.AddForce(input * moveSpeed * speedMultiplierInAir, 0, 0, ForceMode.VelocityChange);
+            _rigidbody.AddForce(input * _moveSpeed * speedMultiplierInAir, 0, 0, ForceMode.VelocityChange);
         }
-        // ������ �� ��� �, ��� �� �� ���������� ����������
-        playerRigidbody.AddForce(-playerRigidbody.velocity.x * friction * speedMultiplierInAir, 0, 0, ForceMode.VelocityChange);
+        _rigidbody.AddForce(-_rigidbody.velocity.x * _friction * speedMultiplierInAir, 0, 0, ForceMode.VelocityChange);
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        // ���������� ����, ��� �� �� ������� �� �����, ������� ����� �������� � ������������� � ���� ��� ����������
         for (int i = 0; i < collision.contactCount; i++)
         {
             float angle = Vector3.Angle(collision.contacts[i].normal, Vector3.up);
-            // ���� ���� �� ������� �� ����� ������ 45 ������ �� �� �����, � ���� �������� ��� �� ����
             if (angle < 45f)
             {
-                _grounded = true;
+                _isGrounded = true;
             }
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        _grounded = false;
+        _isGrounded = false;
     }
 
     //private void OnDestroy()
